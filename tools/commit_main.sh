42e518932f8af1e3205a0019ab655fc9d39b7679
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-17 13:52:22 +0200

    test

---
+++ tools/commit_main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter commit message:"
+read commit_msg
+
+git add .
+
+git commit -m "$commit_msg"
+
+git checkout main
+git merge dev
+git push
+
+git checkout dev
+
+echo "✔ Done!"
